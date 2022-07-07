acfb2632d6c87e8c0c52026a92ad14d8bb8e5026
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-07-07 12:03:09 +0200

    A button will create a randomic sphere

--- src/script.js
+++ src/script.js
@@ -9,6 +9,19 @@ import CANNON from 'cannon' ;
  */
 const gui = new dat.GUI()
 
+const debugObject = {};
+debugObject.createSphere = () => {
+    createSphere(
+        Math.random() * 0.5, 
+        {
+            x: (Math.random() - 0.5) * 3,
+            y: 3,
+            z: (Math.random() * 0.5) * 3
+        }
+    )
+};
+gui.add(debugObject, 'createSphere').name('Create a Sphere');
+
 /**
  * Base
  */
