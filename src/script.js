bfac72ff4caad2315f70660a13ee111d27f1247f
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-07-08 16:39:21 +0200

    hit sound

--- src/script.js
+++ src/script.js
@@ -46,6 +46,21 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Sounds
+ */
+const hitSound = new Audio('/sounds/hit.mp3');
+const playHitSound = (collision) => {
+
+    const impactStrength = collision.contact.getImpactVelocityAlongNormal();
+
+    if(impactStrength > 1.5){
+        hitSound.volume = Math.random();
+        hitSound.currentTime = 0;
+        hitSound.play();
+    }
+}
+
+/**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
@@ -281,6 +296,7 @@ const createBox = (width, height, depth, position) =>{
         material: defaultMaterial
     });
     body.position.copy(position);
+    body.addEventListener('collide', playHitSound);
     world.addBody(body);
 
     // save in objectToUpdate
